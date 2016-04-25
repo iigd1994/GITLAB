84d96829665c3a1240fa1f2d7b6ca1be0f735611
iigd2013 <iigd@hotmail.co.th>
2016-04-25 10:39:33 +0700

    Modified 1.bloodgroup.c

--- 1.bloodgroup.c
+++ 1.bloodgroup.c
@@ -9,7 +9,7 @@ void main()
     scanf("%s", Name);
     do
     {
-      printf("What is your blood type [A, B, AB, or O]? ");
+      printf("What is your blood type [a, b, ab, or o]? ");
       scanf("%s", G);
 
       if (strcmp(G, "A") !=0  &&
